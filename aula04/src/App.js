c78826be5826d92c720666380de48434e7f88cee
pedrodmarchi <78434099+pedrodmarchi@users.noreply.github.com>
2021-10-25 12:23:52 -0300

    vapo

---
+++ aula04/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import man from './assets/homem-feliz-dinheiro.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={man} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
